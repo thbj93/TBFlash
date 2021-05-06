e33b7ee9e1222cf6a185d0a6a689be1d174d7678
thbj93 <thbj93@gmail.com>
2021-05-06 13:01:56 +0200

    Implementeret ID-numre ved tilføjelse af nye kort

--- TBFlash.py
+++ TBFlash.py
@@ -8,8 +8,9 @@ if not os.path.exists(Path.home() / 'TBFlash' / 'collection.xlsx'):
 		os.mkdir(Path.home() / 'TBFlash')
 	wb = openpyxl.Workbook()
 	ws = wb.active
-	ws['A1'] = 'Question'
-	ws['B1'] = 'Answer'
+	ws['A1'] = 'ID'
+	ws['B1'] = 'Question'
+	ws['C1'] = 'Answer'
 	wb.save(Path.home() / 'TBFlash' / 'collection.xlsx')
 
 wb = openpyxl.load_workbook(Path.home() / 'TBFlash' / 'collection.xlsx')
@@ -17,7 +18,8 @@ ws = wb.active
 collection = {}
 
 class FlashCard:
-	def __init__(self, front, back):
+	def __init__(self, cid, front, back):
+		self.cid = cid
 		self.front = front
 		self.back = back
 
@@ -32,12 +34,13 @@ class FlashCard:
 		print('A:', self.back)
 
 def load_cards():
-	for i in range(2, ws.max_row+1):
-		collection['card_'+str(i)] = FlashCard(ws.cell(row=i, column=1).value, ws.cell(row=i, column=2).value)
+	if ws.max_row > 1:
+		for i in range(2, ws.max_row+1):
+			collection['card_'+str(i)] = FlashCard(ws.cell(row=i, column=1), ws.cell(row=i, column=2).value, ws.cell(row=i, column=3).value)
 
 def print_menu():
 	print()
-	print("MENU")
+	print("=== MENU ===")
 	print("1. Add flashcard")
 	print("2. Quiz")
 	print("3. Show flashcards")
@@ -45,32 +48,43 @@ def print_menu():
 	print()
 
 def add_card():
-	print('NEW CARD')
+	print('=== NEW CARD ===')
 	new_row = ws.max_row + 1
 	question = input('Type question: ')
 	answer = input('Type answer: ')
-	ws.cell(row=new_row, column=1).value = question
-	ws.cell(row=new_row, column=2).value = answer
+	ws.cell(row=new_row, column=1).value = get_new_cid()
+	ws.cell(row=new_row, column=2).value = question
+	ws.cell(row=new_row, column=3).value = answer
 	wb.save(Path.home() / 'TBFlash' / 'collection.xlsx')
 	print()
 	load_cards()
 
 def run_quiz():
-	print('QUIZ - ', ws.max_row - 1, 'questions total')
-	for q in range(2, ws.max_row+1):
-		print('Question: ', ws['A' + str(q)].value)
+	print('=== QUIZ - ', ws.max_row - 1, 'questions total ===')
+	for i in range(2, ws.max_row+1):
+		print('Question:', collection['card_'+str(i)].front)
 		user_answer = input('Your answer: ')
-		print('The correct answer is: ', ws['B' + str(q)].value)
+		print('The correct answer is:', collection['card_'+str(i)].back)
 		print()
 
 def show_cards():
-	print('SHOW CARDS')
+	print('=== SHOW CARDS ===')
 	if ws.max_row <= 1:
 		print('No cards to show.')
 	print()
 	for i in range(2, ws.max_row+1):
 		collection['card_'+str(i)].print_card()
 	print()
+
+def get_new_cid():
+	cid_list = []
+	if ws.max_row > 1:
+		for i in range(2, ws.max_row+1):
+			cid_list.append(ws.cell(row=i, column=1).value)
+		new_cid = max(cid_list) + 1
+	elif ws.max_row == 1:
+		new_cid = 1
+	return new_cid
 
 load_cards()
 
